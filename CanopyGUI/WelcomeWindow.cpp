7b4e9f5938fe7bcebfdd95dd390f2df0e2a16a19
Josh Rands <jmrands@mymail.mines.edu>
2019-02-28 20:56:47 -0700

    File browser and line edits for welcome window functioning

--- CanopyGUI/WelcomeWindow.cpp
+++ CanopyGUI/WelcomeWindow.cpp
@@ -7,11 +7,54 @@ WelcomeWindow::WelcomeWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    /**** REMOVE THIS LINE IN RELEASE VERSION ****/
+    ui->evaluateButton->setEnabled(false);
+
     ui->fileFrame->setStyleSheet("background-color: white;");
     ui->browseButton->setStyleSheet("background-color: pale gray;");
+
+    this->currentDirectory = QString("/home");
+    this->fileName = QString("");
 }
 
 WelcomeWindow::~WelcomeWindow()
 {
     delete ui;
+}
+
+void WelcomeWindow::on_browseButton_clicked()
+{
+    if ((this->fileName = QFileDialog::getOpenFileName(this, tr("Open File"),currentDirectory,tr("Images (*.txt *.html *.eml *.mbox)"))) != "")
+    {
+        ui->fileNameLabel->setText(this->fileName);
+    }
+    else
+    {
+        ui->fileNameLabel->setText("No file selected");
+        this->fileName = QString("");
+    }
+
+    this->checkEvaluate();
+}
+
+void WelcomeWindow::checkEvaluate()
+{
+    if (this->fileName != QString("") && ui->suspectNameField->text() != QString("") && ui->warrantNumberField->text() != QString(""))
+    {
+       ui->evaluateButton->setEnabled(true);
+    }
+    else
+    {
+        ui->evaluateButton->setEnabled(false);
+    }
+}
+
+void WelcomeWindow::on_suspectNameField_editingFinished()
+{
+    this->checkEvaluate();
+}
+
+void WelcomeWindow::on_warrantNumberField_editingFinished()
+{
+    this->checkEvaluate();
 }
--- CanopyGUI/WelcomeWindow.h
+++ CanopyGUI/WelcomeWindow.h
@@ -2,6 +2,8 @@
 #define WELCOMEWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+#include <QFileDialog>
 
 namespace Ui {
 class WelcomeWindow;
@@ -15,8 +17,21 @@ public:
     explicit WelcomeWindow(QWidget *parent = 0);
     ~WelcomeWindow();
 
+private slots:
+    void on_browseButton_clicked();
+
+    void on_suspectNameField_editingFinished();
+
+    void on_warrantNumberField_editingFinished();
+
 private:
     Ui::WelcomeWindow *ui;
+
+    QList<QString> validFileTypes;
+    QString currentDirectory;
+    QString fileName;
+
+    void checkEvaluate();
 };
 
 #endif // WELCOMEWINDOW_H
